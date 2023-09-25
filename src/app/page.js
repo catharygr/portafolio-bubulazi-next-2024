73b71d1cd3ef744cd184e7e8feabecb6700aeaf3
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-25 13:23:27 +0100

    Probando el prettier

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,12 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <h1>Portafolio</h1>
+
       <h2> holaaa hola </h2>
+
       <h4> problemas con el deploy </h4>
+
+      <h6> hola </h6>
     </main>
   );
 }
