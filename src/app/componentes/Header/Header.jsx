9ecdb806b6786ee708de03523c8db80367bc837f
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-26 13:52:45 +0100

    Trabajando en el componente Header y CSS

--- src/app/componentes/Header/Header.jsx
+++ src/app/componentes/Header/Header.jsx
@@ -2,7 +2,7 @@ import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <header>
+    <header className={styles.header}>
       <h1>Header</h1>
     </header>
   );
