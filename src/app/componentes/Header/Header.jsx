92d658408681a153dff5ac3968a2a6396de135f1
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-26 12:27:52 +0100

    Creado el componente Header, CSS, index

---
+++ src/app/componentes/Header/Header.jsx
@@ -0,0 +1,9 @@
+import styles from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Header from "@/app/componentes/Header";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Portafolio</h1>
-    </main>
+    <>
+      <Header />
+    </>
   );
 }
