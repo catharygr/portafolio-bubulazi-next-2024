e2c2a1755f2a99fcf54cd2e1fb8f736dbb80deb3
catharygr <103951447+catharygr@users.noreply.github.com>
2023-09-27 12:50:28 +0100

    Trabajando en Header y CSS

--- src/app/componentes/Header/Header.jsx
+++ src/app/componentes/Header/Header.jsx
@@ -1,9 +1,22 @@
 import styles from "./Header.module.css";
+im;
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1>Header</h1>
+      <div className={`wrapper ${styles.container}`}>
+        <p className={styles.logo}>{"{this.cataryGarciaRivas}"}</p>
+        <div className={styles.navContainer}>
+          <ul className={styles.ul}>
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Quienes somos</li>
+          </ul>
+          <nav>
+            <p>Mi Nav</p>
+          </nav>
+        </div>
+      </div>
     </header>
   );
 }
