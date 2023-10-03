31131dee3d70370c7c50d553a853ab23d9b16353
catharygr <103951447+catharygr@users.noreply.github.com>
2023-10-03 11:31:30 +0100

    Trabajando en el componente Hero

--- src/app/componentes/Hero/Hero.jsx
+++ src/app/componentes/Hero/Hero.jsx
@@ -1,15 +1,16 @@
 import Image from "next/image";
-import Emoji from "@/app/imagen/Hero/caty-emoji.png";
+import EmojiCaty from "@/app/assets/imagen/generales/caty-emoji.png";
 import styles from "./Hero.module.css";
 
-export default function Hero() {
+// Tipo con imagen y tipo sin imagen
+export default function Hero({ type }) {
   return (
     <section>
-      <div className={`wrapper  ${styles.heroContainer}`}>
+      <div className={`wrapper  ${styles.containerTipoImagen}`}>
         <div
-          className={`${styles.rotateCenter} ${styles.heroEmoji} fondo-acento-claro`}
+          className={`${styles.rotateCenter} ${styles.tipoImagen} fondo-acento-claro`}
         >
-          <Image src={Emoji} alt="emoji de mi" />
+          <Image className={styles.img} src={EmojiCaty} alt="emoji de mi" />
         </div>
         <h1 className="fs-hero f-str-75 f-wei-600 lh-small color-oscuro">
           Cathy, desarrolladora FrontEnd y algo más...
