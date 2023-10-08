bc9b8c9e64c6d931329b0871f0772ff2f1ea68e0
catharygr <103951447+catharygr@users.noreply.github.com>
2023-10-08 13:19:11 +0100

    Trabajando con importaciones de contenidos

--- src/app/componentes/SubHero/SubHero.jsx
+++ src/app/componentes/SubHero/SubHero.jsx
@@ -2,24 +2,16 @@ import Link from "next/link";
 import styles from "./SubHero.module.css";
 
 //Type: single, single-link, doble-una-lista, doble-tres-listas
-export default function SubHero({ type, color, children }) {
+export default function SubHero({ type, content }) {
+  const { titleSuperior, titleInferior, contentSuperior, contentInferior } =
+    content;
   return (
     <>
       <section className={styles.sectionSuperior}>
         <div className={`wrapper ${styles.wrapperSuperior}`}>
-          <h2 className={styles.h2}>Sobre mi</h2>
+          <h2 className={styles.h2}>{titleSuperior}</h2>
           <div className={styles.contenidoSuperior}>
-            <p>
-              ¡Hola! Iniciada en el mundo de la programación como junior, con
-              muchas ganas de llevar a cabo todo lo aprendido en diferentes
-              cursos, como en{" "}
-              <Link href="https://freecodecamp.org">FreeCodeCamp</Link>,{" "}
-              <Link href="https://platzi.com">Platzi</Link> y{" "}
-              <Link href="https://scrimba.com">Scrimba</Link> de CSS, HTML,
-              JavaScript, React 18 y React Router 6. Con mucha disposición,
-              determinación y voluntad para seguir aprendiendo. Espero a que os
-              animéis a contactarme. Gracias de antemano.
-            </p>
+            <p>{contentSuperior}</p>
             {type === "single-link" && (
               <Link
                 href={"/"}
@@ -35,14 +27,11 @@ export default function SubHero({ type, color, children }) {
       {type === "doble-una-lista" && (
         <section className={styles.sectionInferior}>
           <div className={`wrapper ${styles.wrapperInferior}`}>
-            <h2 className={styles.h2}>Mis habilidades</h2>
+            <h2 className={styles.h2}>{titleInferior}</h2>
             <ul className={styles.unaListaUl}>
-              <li>JavaScript ES6</li>
-              <li>React 18</li>
-              <li>React Router 6</li>
-              <li>Git & GitHub</li>
-              <li>CSS & HTML</li>
-              <li>Firebase</li>
+              {contentInferior.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
             </ul>
           </div>
         </section>
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Hero from "./componentes/Hero";
 import SubHero from "./componentes/SubHero";
 import styles from "./page.module.css";
 import EmojiCaty from "@/app/assets/imagen/generales/caty-emoji.png";
+import { SOBRE_MI } from "@/app/contenido/home-page/sobreMi";
 
 export default function Home() {
   return (
@@ -13,7 +14,10 @@ export default function Home() {
         title="Cathy, desarrolladora FrontEnd y algo más..."
         alt="Mi emoji frente de un ordenador"
       />
-      <SubHero type="single-link" />
+      <SubHero
+        type="doble-una-lista"
+        content={SOBRE_MI}
+      />
     </main>
   );
 }
