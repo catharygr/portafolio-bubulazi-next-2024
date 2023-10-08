58dc772a89f19cca7572f64efe1ee7270c0b396b
catharygr <103951447+catharygr@users.noreply.github.com>
2023-10-08 17:57:55 +0100

    Trabajando con el componente CardConImg y CSS

--- src/app/componentes/CardConImg/CardConImg.jsx
+++ src/app/componentes/CardConImg/CardConImg.jsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 import styles from "./CardConImg.module.css";
 import Image from "next/image";
 
 export default function CardConImg({ content }) {
-  const { imageURL, title, description, url, linkText } = content;
+  const { imageURL, title, description, url, linkTexto } = content;
   console.log(imageURL);
   return (
     <div className={styles.container}>
@@ -11,6 +12,9 @@ export default function CardConImg({ content }) {
           src={imageURL}
           alt=""
         />
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <Link href={url}>{linkTexto}</Link>
       </div>
     </div>
   );
