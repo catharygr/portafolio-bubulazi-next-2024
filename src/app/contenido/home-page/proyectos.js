1162cca5ad332fd1f9dd7cbf71817ae66dad3d44
catharygr <103951447+catharygr@users.noreply.github.com>
2023-11-07 15:09:54 +0000

    Trabajando en el componente Card

--- src/app/contenido/home-page/proyectos.js
+++ src/app/contenido/home-page/proyectos.js
@@ -1,5 +1,6 @@
-import formularioMultiPaginas from "@/app/contenido/home-page/imagen/multipagina-tarjeta.png";
-import bubulaziPortafolio from "@/app/contenido/home-page/imagen/bubulazi-tarjeta.jpg";
+import formularioMultiPaginas from "@/app/contenido/home-page/imagenes/multipagina-tarjeta.png";
+import bubulaziPortafolio from "@/app/contenido/home-page/imagenes/bubulazi-tarjeta.jpg";
+import recordatorios from "@/app/contenido/home-page/imagenes/recordatorio-tarjeta.jpeg";
 
 export const PROYECTOS = [
   {
@@ -19,41 +20,41 @@ export const PROYECTOS = [
     imageURL: formularioMultiPaginas,
   },
   {
-    title: "Formulario Multi-páginas",
+    title: "Recordatorio",
     description:
-      "Página web con diseño resposive desarrodada Con React, Hooks más usados, firebase Auth, Storage y RealTime Database como Back-end.",
+      "Aplicación desarrollada para móviles con tecnologías más modernas como son: React 18, React Router-6, Firebase Auth, Firestore como Back-end",
+    url: "/",
+    linkTexto: "Ver proyecto",
+    imageURL: recordatorios,
+  },
+  {
+    title: "ChatGTP - Chat App",
+    description:
+      "Conexión con la API de OpenAI para crear un chatbot. Firebase RealTime Database para Back-end y Auth. Netlify env variable para ocultar API key. Netlify función para conectar con OpeAI y obtener las respuestas.",
     url: "/",
     linkTexto: "Ver proyecto",
     imageURL: formularioMultiPaginas,
   },
   {
-    title: "Formulario Multi-páginas",
+    title: "Sarajevo Tours",
     description:
-      "Página web con diseño resposive desarrodada Con React, Hooks más usados, firebase Auth, Storage y RealTime Database como Back-end.",
+      "Mi primer proyecto desarrollado con JavaScript, práctica React-18, React-Router 6, Firebase Auth, irestore como Back-end y los Hook más usados.",
     url: "/",
     linkTexto: "Ver proyecto",
     imageURL: formularioMultiPaginas,
   },
   {
-    title: "Formulario Multi-páginas",
+    title: "Clínica dental Blancodent",
     description:
-      "Página web con diseño resposive desarrodada Con React, Hooks más usados, firebase Auth, Storage y RealTime Database como Back-end.",
+      "La página web de una clínica dental (cliente comercial) desarrollada con CSS, HTML y un poco de JS. La web original estaba en Squarespace y la he transferido a Netlify respectando el diseño original.",
     url: "/",
     linkTexto: "Ver proyecto",
     imageURL: formularioMultiPaginas,
   },
   {
-    title: "Formulario Multi-páginas",
+    title: "Pac-Man",
     description:
-      "Página web con diseño resposive desarrodada Con React, Hooks más usados, firebase Auth, Storage y RealTime Database como Back-end.",
-    url: "/",
-    linkTexto: "Ver proyecto",
-    imageURL: formularioMultiPaginas,
-  },
-  {
-    title: "Formulario Multi-páginas",
-    description:
-      "Página web con diseño resposive desarrodada Con React, Hooks más usados, firebase Auth, Storage y RealTime Database como Back-end.",
+      "Mi primer proyecto desarrollado con JavaScript, CSS y HTML. Inspirada en el juego de Pac-Man.Para jugar tiene que ser desde un ordenador porque está hecho para teclado =>",
     url: "/",
     linkTexto: "Ver proyecto",
     imageURL: formularioMultiPaginas,
