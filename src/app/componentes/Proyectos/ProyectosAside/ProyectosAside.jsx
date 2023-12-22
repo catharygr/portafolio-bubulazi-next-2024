6843dea394c3938ff67106014c17ba2a7285c705
catharygr <103951447+catharygr@users.noreply.github.com>
2023-12-22 14:02:51 +0000

    Trabajando en ProyectosAside.module.css

--- src/app/componentes/Proyectos/ProyectosAside/ProyectosAside.jsx
+++ src/app/componentes/Proyectos/ProyectosAside/ProyectosAside.jsx
@@ -1,7 +1,17 @@
-export default function ProyectosAside() {
+import styles from "./ProyectosAside.module.css";
+
+export default function ProyectosAside({ content }) {
+  const { bloqueUno, bloqueDos, bloqueTres } = content;
   return (
-    <aside>
-      <h3>ProyectosAside</h3>
+    <aside className="wrapper">
+      <div className={styles.gridContainer}>
+        <div className={`flow ${styles.bloqueUno}`}>
+          <h3 className={styles.h3}>{bloqueUno.title}</h3>
+          {bloqueUno.content.map((item, index) => (
+            <p key={index}>{item}</p>
+          ))}
+        </div>
+      </div>
     </aside>
   );
 }
