a4d2cfa17e2881c280d25ea2c31bf150c88ee68a
catharygr <103951447+catharygr@users.noreply.github.com>
2023-12-22 10:31:29 +0000

    Trabajando en en contenido de la imagen del componente ProyectosMain

--- src/app/componentes/Proyectos/ProyectosMain/ProyectosMain.jsx
+++ src/app/componentes/Proyectos/ProyectosMain/ProyectosMain.jsx
@@ -1,4 +1,7 @@
 import styles from "./ProyectosMain.module.css";
+import Image from "next/image";
+import paginaCorta from "../../../contenido/formulario-multi-paginas/imagenes/pagina-corta.png";
+import paginaLarga from "../../../contenido/formulario-multi-paginas/imagenes/pagina-larga.png";
 
 export default function ProyectosMain({ content }) {
   const { bloqueUno, bloqueDos, bloqueTres, bloqueCuatro, bloqueCinco } =
@@ -13,6 +16,16 @@ export default function ProyectosMain({ content }) {
         <div className={styles.bloqueDos}>
           <h3>{bloqueDos.title}</h3>
           <p>{bloqueDos.content}</p>
+        </div>
+        <div>
+          <picture>
+            <source media="(max-width: 49.999rem)" srcset={paginaCorta.src} />
+            <source media="(min-width: 50rem)" srcset={paginaLarga.src} />
+            <Image
+              src={paginaCorta}
+              alt="Imágenes del formulario de múltiples pasos"
+            />
+          </picture>
         </div>
       </div>
     </main>
