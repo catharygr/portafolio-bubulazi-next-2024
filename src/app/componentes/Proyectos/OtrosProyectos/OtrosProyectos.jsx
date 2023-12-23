a05e426ff5587759807a79798fc32a90be2c9867
catharygr <103951447+catharygr@users.noreply.github.com>
2023-12-23 13:32:31 +0000

    Trabajando en la lógica del componente OtrosProyectos

--- src/app/componentes/Proyectos/OtrosProyectos/OtrosProyectos.jsx
+++ src/app/componentes/Proyectos/OtrosProyectos/OtrosProyectos.jsx
@@ -1,12 +1,20 @@
 import styles from "./OtrosProyectos.module.css";
 
 export default function OtrosProyectos({ content, id }) {
+  let primerProyecto = "";
   for (let i = 0; i < 2; i++) {
     const ranDomNum = Math.floor(Math.random() * content.length);
-    if(content[ranDomNum].id === id) {
+    if (content[ranDomNum].id === id) {
       i--;
-      
-    console.log(ranDomNum);
+      continue;
+    }
+    if (primerProyecto === content[ranDomNum].id) {
+      i--;
+      continue;
+    }
+    primerProyecto = content[ranDomNum].id;
+
+    console.log(primerProyecto);
   }
   return (
     <div>
