3c3d3ae53ebfd3a02c2ccdfe4f6c7867bdbc6d4e
catharygr <103951447+catharygr@users.noreply.github.com>
2023-12-23 13:20:31 +0000

    Trabajando en la lógica del componente OtrosProyectos

--- src/app/componentes/Proyectos/OtrosProyectos/OtrosProyectos.jsx
+++ src/app/componentes/Proyectos/OtrosProyectos/OtrosProyectos.jsx
@@ -1,8 +1,13 @@
-import Card from "../../Card";
-import Link from "next/link";
 import styles from "./OtrosProyectos.module.css";
 
-export default function OtrosProyectos({ content }) {
+export default function OtrosProyectos({ content, id }) {
+  for (let i = 0; i < 2; i++) {
+    const ranDomNum = Math.floor(Math.random() * content.length);
+    if(content[ranDomNum].id === id) {
+      i--;
+      
+    console.log(ranDomNum);
+  }
   return (
     <div>
       <h1>Otros proyectos</h1>
