b2978d4f0bef3bc5bbb8e558c4f470d4f9f92046
catharygr <103951447+catharygr@users.noreply.github.com>
2023-12-27 12:15:03 +0000

    Trabajando en el enlace proyectos

---
+++ src/app/proyectos/page.js
@@ -0,0 +1,16 @@
+import Hero from "@/app/componentes/Hero";
+import HomeProyecto from "@/app/componentes/HomeProyecto";
+import { PROYECTOS } from "@/app/contenido/home-page/proyectos";
+
+export default function PageProyectos() {
+  return (
+    <>
+      <Hero
+        type="sin-imagen"
+        about="Mis proyectos personales"
+        title="Todos los proyectos"
+      />
+      <HomeProyecto content={PROYECTOS} />
+    </>
+  );
+}
