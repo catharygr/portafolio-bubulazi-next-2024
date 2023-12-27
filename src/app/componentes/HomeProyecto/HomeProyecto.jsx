7b0b0b1ffdee808e6d32000d29c34a79b5494f3d
catharygr <103951447+catharygr@users.noreply.github.com>
2023-12-27 12:36:58 +0000

    Trabajando en el componente HomeProyectos

--- src/app/componentes/HomeProyecto/HomeProyecto.jsx
+++ src/app/componentes/HomeProyecto/HomeProyecto.jsx
@@ -1,31 +1,61 @@
 import Card from "@/app/componentes/Card/Card";
 import styles from "./HomeProyecto.module.css";
 import Link from "next/link";
+import Hero from "@/app/componentes/Hero/Hero";
 
 // Tipo de contenido: "full" o "reduced"
 export default function HomeProyecto({ content, type }) {
-  const ultimosProyectos = content.slice(0, 3);
-  const otrosProyectos = content.slice(3);
+  if (type === "reduced") {
+    const ultimosProyectos = content.slice(0, 3);
+    const otrosProyectos = content.slice(3, 7);
 
-  return (
-    <section>
-      <div className={`wrapper flow ${styles.proyectoWrapper}`}>
-        <h2 className={styles.h2}>Últimos proyectos</h2>
-        <div className={styles.cardsContainer}>
-          {ultimosProyectos.map((proyecto) => (
-            <Card key={proyecto.url} content={proyecto} type="conImg" />
-          ))}
+    return (
+      <section>
+        <div className={`wrapper flow ${styles.proyectoWrapper}`}>
+          <h2 className={styles.h2}>Últimos proyectos</h2>
+          <div className={styles.cardsContainer}>
+            {ultimosProyectos.map((proyecto) => (
+              <Card key={proyecto.url} content={proyecto} type="conImg" />
+            ))}
+          </div>
+          <h2 className={styles.h2}>Otros proyectos</h2>
+          <div className={styles.cardSinImg}>
+            {otrosProyectos.map((proyecto) => (
+              <Card key={proyecto.url} content={proyecto} type="sinImg" />
+            ))}
+          </div>
+          <Link className="link-btn" href="/proyectos">
+            Ver todos los proyectos
+          </Link>
         </div>
-        <h2 className={styles.h2}>Otros proyectos</h2>
-        <div className={styles.cardSinImg}>
-          {otrosProyectos.map((proyecto) => (
-            <Card key={proyecto.url} content={proyecto} type="sinImg" />
-          ))}
+      </section>
+    );
+  }
+  if (type === "full") {
+    const ultimosProyectos = content.slice(0, 3);
+    const otrosProyectos = content.slice(3);
+
+    return (
+      <section>
+        <div className={`wrapper flow ${styles.proyectoWrapper}`}>
+          <Hero
+            type="sin-imagen"
+            about="Mis proyectos personales"
+            title="Todos los proyectos"
+          />
+          <div className={styles.cardsContainer}>
+            {ultimosProyectos.map((proyecto) => (
+              <Card key={proyecto.url} content={proyecto} type="conImg" />
+            ))}
+          </div>
+
+          <div className={styles.cardSinImg}>
+            {otrosProyectos.map((proyecto) => (
+              <Card key={proyecto.url} content={proyecto} type="sinImg" />
+            ))}
+          </div>
         </div>
-        <Link className="link-btn" href="/proyectos">
-          Ver todos los proyectos
-        </Link>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
--- src/app/proyectos/page.js
+++ src/app/proyectos/page.js
@@ -5,11 +5,6 @@ import { PROYECTOS } from "@/app/contenido/home-page/proyectos";
 export default function PageProyectos() {
   return (
     <>
-      <Hero
-        type="sin-imagen"
-        about="Mis proyectos personales"
-        title="Todos los proyectos"
-      />
       <HomeProyecto content={PROYECTOS} type="full" />
     </>
   );
