bdfe89e4a77b04ba17d7bd3685a97d1d6060d179
catharygr <103951447+catharygr@users.noreply.github.com>
2024-01-11 10:32:41 +0000

    Importación de la imágenes en el componente ProyectosMain

--- src/app/componentes/Proyectos/ProyectosMain/ProyectosMain.jsx
+++ src/app/componentes/Proyectos/ProyectosMain/ProyectosMain.jsx
@@ -38,12 +38,7 @@ export default function ProyectosMain({ content }) {
             <p key={index}>{item}</p>
           ))}
         </div>
-        <div className={styles.bloqueCinco}>
-          <Image
-            src={bloqueCinco.imgTres}
-            alt="Imágenes del formulario de múltiples pasos"
-          />
-        </div>
+        <div className={styles.bloqueCinco}></div>
       </div>
     </main>
   );
