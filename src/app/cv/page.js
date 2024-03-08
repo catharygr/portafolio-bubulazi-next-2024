388566254f26b697b07e3531247eeccdf3f794a5
catharygr <103951447+catharygr@users.noreply.github.com>
2024-03-08 13:07:21 +0000

    Trabajando en detalles

--- src/app/cv/page.js
+++ src/app/cv/page.js
@@ -1,26 +1,7 @@
 import Hero from "@/app/componentes/Hero";
-import SubHero from "@/app/componentes/SubHero";
 import imagenCV from "@/app/assets/imagen/generales/foto-uno.png";
 import styles from "./page.module.css";
-
-export const SOBRE_MI = {
-  titleSuperior: "Sobre mí",
-  contentSuperior: `
-  ¡Hola! Iniciada en el mundo de la programación como junior, con muchas ganas de llevar a cabo todo lo aprendido en diferentes cursos, como en FreeCodeCamp, Platzi y Scrimba de CSS, HTML, JavaScript, React 18 y React Router 6. Con mucha disposición, determinación y voluntad para seguir aprendiendo. Espero a que os animéis a contactarme. Gracias de antemano.`,
-  titleInferior: "Habilidades",
-  contentInferior: [
-    "Next.js",
-    "React",
-    "React Router",
-    "JavaScript",
-    "ES6",
-    "CSS & HTML",
-    "Git",
-    "Firebase",
-    "Netlify",
-    "Vercel",
-  ],
-};
+import Link from "next/link";
 
 export default function CVPage() {
   return (
@@ -30,24 +11,76 @@ export default function CVPage() {
         title="Curriculum Vitae: Catary García Rivas."
         image={imagenCV}
       />
-
-      <section className="wrapper flow">
-        <p>
-          <span className="bold-text">Desarrolladora Web Front-End</span> junior
-          con conocimientos de{" "}
-          <span className="bold-text">React, Nextjs, React Router</span>,
-          JavaScript, CSS, HTML, Firebase y{" "}
-          <span className="bold-text">Accesibilidad</span> web.
-        </p>
-        <p>
-          A lo largo de mi trayectoria personal y profesional, he demostrado una
-          firme determinación para alcanzar mis objetivos. Como el de cambiar la
-          profesión de económica a desalladora web. Aprendo rápidamente y estoy
-          comprometida con seguir evolucionando en este campo, buscando
-          oportunidades como desarrolladora Front-End en una empresa que
-          comparta mis valores y donde pueda seguir aprendiendo y aportando.
-        </p>
+      <section className={`wrapper  ${styles.containerQuery} `}>
+        <div className={`${styles.flexContainer} ${styles.regularCard}`}>
+          <h2 className={styles.h2}>Resumen</h2>
+          <div className={`flow ${styles.jobDetails}`}>
+            <div>
+              <h3 className={styles.h3}>Profesión</h3>
+              <p>
+                <span className="bold-text">Desarrolladora Web Front-End</span>{" "}
+                junior con conocimientos de{" "}
+                <span className="bold-text">React, Nextjs, React Router</span>,
+                JavaScript, CSS, HTML, Firebase y{" "}
+                <span className="bold-text">Accesibilidad</span> web.
+              </p>
+            </div>
+            <div>
+              <h3 className={styles.h3}>Sobre mi</h3>
+              <p>
+                A lo largo de mi trayectoria personal y profesional, he
+                demostrado una firme determinación para alcanzar mis objetivos.
+                Como el de cambiar la profesión de económica a desalladora web.
+                Aprendo rápidamente y estoy comprometida con seguir
+                evolucionando en este campo, buscando oportunidades como
+                desarrolladora Front-End en una empresa que comparta mis valores
+                y donde pueda seguir aprendiendo y aportando.
+              </p>
+            </div>
+          </div>
+        </div>
       </section>
+      <section className={`wrapper  ${styles.containerQuery} `}>
+        <div className={`${styles.flexContainer} ${styles.invertedCard}`}>
+          <h2 className={styles.h2}>Detalles</h2>
+          <div className={`flow ${styles.jobDetails}`}>
+            <div>
+              <h3 className={styles.h3}>Datos de contactos</h3>
+              <p>
+                <Link href="/#contacto-form">Formulario de contacto</Link>
+              </p>
+              <p>
+                <a
+                  target="_blank"
+                  href={"https://www.linkedin.com/in/hyuik"}
+                >
+                  LinkedIn
+                </a>
+              </p>
+              <p>
+                <a
+                  target="_blank"
+                  href={"https://github.com/catharygr"}
+                >
+                  GitHub
+                </a>
+              </p>
+              <p>Disponibilidad geográfica e inmediata</p>
+            </div>
+            <div>
+              <h3 className={styles.h3}>Clinica Dental Blancodent</h3>
+              <p>Freelance | 2022 - Actualidad</p>
+              <p>Desarrollo página web: blancodent.com.</p>
+            </div>
+            <div>
+              <h3 className={styles.h3}>Clinica Dental Blancodent</h3>
+              <p>Freelance | 2022 - Actualidad</p>
+              <p>Desarrollo página web: blancodent.com.</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
       <section className={`wrapper  ${styles.containerQuery} `}>
         <div className={`${styles.flexContainer} ${styles.regularCard}`}>
           <h2 className={styles.h2}>Experiencia Laboral</h2>
