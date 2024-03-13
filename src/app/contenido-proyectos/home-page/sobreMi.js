d6bf761ec7ef966f719d3d4050607577efdba3be
catharygr <103951447+catharygr@users.noreply.github.com>
2024-03-13 22:53:23 +0000

    Terminado con los enlaces

--- src/app/contenido-proyectos/home-page/sobreMi.js
+++ src/app/contenido-proyectos/home-page/sobreMi.js
@@ -1,7 +1,7 @@
 export const SOBRE_MI = {
   titleSuperior: "Sobre mí",
 
-  contentSuperior: `¡Hola! Iniciada en el mundo de la programación como junior, con muchas ganas de llevar a cabo todo lo aprendido en diferentes cursos, como en las plataformas interactivas: <a href="https://qualentum.com" target="_blank">Qualentum</a>, <a href="https://www.joshwcomeau.com/"target="_blank">Josh W Comeau's</a>, <a href="https://www.sarasoueidan.com/" target="_blank">Practical Accessibility con Sara Soueidan</a>, <a href="https://platzi.com" target="_blank">Platzi</a>, <a href="https://scrimba.com/" target="_blank">Scrimba</a>, <a href="https://www.freecodecamp.org/" target="_blank">FreeCodeCamp</a> de React, Netxjs, Redux, Accesibilidad. Con mucha disposición, determinación y voluntad para seguir aprendiendo. Espero a que os animéis a contactarme. Gracias de antemano.`,
+  contentSuperior: `¡Hola! Iniciada en el mundo de la programación como junior, con muchas ganas de llevar a cabo todo lo aprendido en diferentes cursos, como en las plataformas interactivas: <a href="https://qualentum.com" target="_blank">Qualentum</a>, <a href="https://www.joshwcomeau.com/"target="_blank">Josh W Comeau's</a>, <a href="https://www.sarasoueidan.com/" target="_blank">Practical Accessibility con Sara Soueidan</a>, <a href="https://platzi.com" target="_blank">Platzi</a>, <a href="https://scrimba.com/" target="_blank">Scrimba</a>, <a href="https://www.freecodecamp.org/" target="_blank">FreeCodeCamp</a>. Con mucha disposición, determinación y voluntad para seguir aprendiendo. Espero a que os animéis a contactarme. Gracias de antemano.`,
   titleInferior: "Habilidades",
   contentInferior: [
     "Next.js",
