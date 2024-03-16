2f8ae7f639cbfca1472c8982f01eb5e3a8b2452e
catharygr <103951447+catharygr@users.noreply.github.com>
2024-03-16 11:03:09 +0000

    Corrección de contenidos

--- src/app/contenido-proyectos/mi-tienda/contenido.js
+++ src/app/contenido-proyectos/mi-tienda/contenido.js
@@ -16,7 +16,7 @@ export const contenidoDePagina = {
       "¡Bienvenido a E-commerce: Mi tienda! Con formularios controlados, enrutamientos, carrito de compra y otras funcionalidades. Esta página web ofrece una experiencia de compra ficticia. Inspirado y orientado por la experiencia del profesor del bootcamp. Hemos diseñado esta página para brindar una simulación de compra en línea. ",
     titleInferior: ["Tipo", "Herramientas", "En vivo"],
     contentInferior: {
-      colUno: ["Web App E-commerce Mi Tienda"],
+      colUno: ["Web App", "Comercio electrónico"],
       colDos: [
         "React",
         "React Router",
@@ -26,7 +26,7 @@ export const contenidoDePagina = {
         "Axios",
         "Git & GitHub",
         "Firebase",
-        "Vercel",
+        "Netlify",
       ],
 
       colTres: ["https://qualentum-ecommerce-catary.netlify.app"],
@@ -46,7 +46,7 @@ export const contenidoDePagina = {
       content: [
         "Como alumna en este proyecto E-commerce, mi papel ha sido fundamentalmente el de aprender y aplicar los conocimientos adquirido.",
         "A lo largo del sprint, mi enfoque ha sido seguir las pautas establecidas, mientras exploraba cómo implementarlas en la práctica. En cada etapa del desarrollo, he buscado la orientación del profesor para comprender mejor los conceptos y abordar los desafíos que han surgido. ",
-        "Gracias a todo lo que he aprendido, he podido encontrar las soluciones adecuadas y superar los desafíos planteados, lo que me ha ayudado a desarrollar habilidades para comprender los principios fundamentales de la aplicación. Estos conocimientos no solo me han permitido alcanzar los objetivos del proyecto, sino también consolidarlos.",
+        "Gracias a todo lo que he aprendido, he podido encontrar las soluciones adecuadas y superar los desafíos planteados. Estos conocimientos no solo me han permitido alcanzar los objetivos del proyecto, sino también consolidarlos.",
       ],
     },
     bloqueTres: {
@@ -60,17 +60,17 @@ export const contenidoDePagina = {
 
         "Custom Hooks: Para extraer lógica compleja y reutilizable en la app. Esto me ha permitido mantener el código limpio y modular.",
 
-        "React Router: Con la función createBrowserRouter me ha permitido definir rutas y enlaces entre diferentes páginas de manera fácil y eficiente.",
+        "React Router: Con la función createBrowserRouter me ha permitido definir rutas y enlaces entre diferentes páginas.",
 
         "Axios: Para realizar solicitudes HTTP a nuestro servidor y generar un CRUD para interactuar con los datos de nuestra aplicación.",
 
-        "Redux y Redux Toolkit: He modificado nuestra app para pasarla totalmente la gestión del estado a Redux y Redux Toolkit. Utilizando métodos como createAsyncThunk y createSlice para definir acciones asíncronas y slices de estado respectivamente, simplificando la gestión del estado global de nuestra aplicación.",
+        "Redux y Redux Toolkit: He modificado nuestra app para pasar la gestión del estado a Redux y Redux Toolkit. He utilizado métodos como createAsyncThunk y createSlice para definir acciones asíncronas y slices de estado respectivamente, simplificando la gestión del estado global de nuestra aplicación.",
 
-        "React Hook Form: He imprementado validaciones y proporcionado feedback sobre posibles errores, como datos incorrectos en los formularios de registro e inicio de sesión.",
+        "React Hook Form: He imprementado validaciones y proporcionado feedback sobre posibles errores, como datos incorrectos en los formularios de edición de productos e inicio de sesión.",
 
-        "He utilizado Axios para crear un CRUD y JSON Server como servidor de desarrollo. Luego he  pasado los datos a Firebase y he compilado la app en Netlify.",
+        "JSON Server & Firebase: Se ha utilizado como servidor durante desarrollo. Luego he pasado los datos a Firebase y he compilado la app en Netlify.",
 
-        "Git & GitHub: Commits, Branches, Merge, Restore, Push.",
+        "Git & GitHub: Commits, Branches, Merge, Restore, Push. También conexión con Netlify para despliegue continuo.",
       ],
     },
     bloqueCinco: {
@@ -93,8 +93,7 @@ export const contenidoDePagina = {
     bloqueTres: {
       title: "Lecciones aprendidas",
       content: [
-        "Durante el tiempo que estuve trabajando en la aplicación, he aprendido muchas cosas que realmente han enriquecido mi experiencia en el desarrollo de software. El cambio de React a Redux me ha dejado claro lo esencial que es planificar con cuidado y hacer la transición de forma gradual.",
-        "Para la adaptabilidad a diferentes dispositivos, he utilizado técnicas como CSS media queries. ",
+        "Durante el tiempo que estuve trabajando en la app, he aprendido muchas cosas que me serviran en el futuro en el desarrollo de software. El cambio de React a Redux me ha dejado claro lo esencial que es planificar con cuidado y hacer la transición de forma gradual.",
         "Todo este aprendizaje ha hecho que mis habilidades técnicas se fortalezcan, que mejore mi capacidad para trabajar en equipo, y me sienta más preparada para enfrentar los retos que se presenten a partir de ahora en adelante.",
       ],
     },
