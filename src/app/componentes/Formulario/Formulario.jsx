45c9920c8b16d3b288edf28041326c2b82553cab
catharygr <103951447+catharygr@users.noreply.github.com>
2024-03-22 12:38:47 +0000

    Probando que  formulario

--- src/app/componentes/Formulario/Formulario.jsx
+++ src/app/componentes/Formulario/Formulario.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 export default function Formulario() {
   const [form, setForm] = useState({
-    nombre: "",
+    name: "",
     email: "",
     mensaje: "",
     formMessage: "",
