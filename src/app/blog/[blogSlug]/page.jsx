8a50ac107be95c15380b9a675f4b0d28a14ab933
catharygr <103951447+catharygr@users.noreply.github.com>
2024-10-07 12:48:40 +0100

    Trabajando en la carpeta dinámica blogSlug

---
+++ src/app/blog/[blogSlug]/page.jsx
@@ -0,0 +1,68 @@
+import "./blog.css";
+import { loadBlogPost } from "@/app/utilidades/node-helpers/node-fs-helpers.js";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import { formatDate } from "@/app/utilidades/varias-utilidades";
+import { BASE_METADATA } from "@/app/utilidades/constante";
+import { Code } from "bright";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+Code.theme = "dracula-soft";
+Code.lineNumbers = true;
+
+//  Check lo de React.cache en node-helpers para no ejecutar dos veces la functión
+export async function generateMetadata({ params }) {
+  const draftPostData = await loadBlogPost(params.blogSlug);
+
+  if (!draftPostData) {
+    return null;
+  }
+
+  const { frontmatter } = draftPostData;
+
+  return {
+    title: `${frontmatter.title} • ${BASE_METADATA.title}`,
+    description: frontmatter.abstract,
+  };
+}
+
+export default async function DraftPage({ params }) {
+  const draftsPostData = await loadBlogPost(params.blogSlug);
+
+  if (!draftsPostData) {
+    return notFound();
+  }
+
+  const { frontmatter, content } = draftsPostData;
+
+  return (
+    <main
+      lang={frontmatter.language}
+      className="wrapper drafts-styles"
+    >
+      <h1>{frontmatter.title}</h1>
+      <div className="draft-details">
+        <time>
+          Last edit: {formatDate(frontmatter.lastEdited, frontmatter.language)}
+        </time>
+        <p>
+          First edit:{" "}
+          <strong>
+            {formatDate(frontmatter.initialDate, frontmatter.language)}
+          </strong>{" "}
+          • Stage: <strong>{frontmatter.stage}</strong> • Certainty:{" "}
+          <strong>{frontmatter.certainty}</strong> • Importance:{" "}
+          <strong>{frontmatter.importance}</strong>
+        </p>
+      </div>
+      <article className="wrapper">
+        <MDXRemote
+          source={content}
+          components={{
+            pre: Code,
+            a: Link,
+          }}
+        />
+      </article>
+    </main>
+  );
+}
