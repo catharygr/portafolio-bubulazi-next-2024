9b756166bca67e6f5601f948d12bb354974ad466
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-05 13:50:13 +0000

    Trabajando con el hook useState()

--- src/app/componentes/BlogPaginationList/BlogPaginationList.jsx
+++ src/app/componentes/BlogPaginationList/BlogPaginationList.jsx
@@ -1,7 +1,10 @@
 "use client";
 import BlogCard from "../BlogCard";
+import styles from "./BlogPaginationList.module.css";
+import { useState } from "react";
 
 export default function BlogPaginationList({ blogs }) {
+  const [currentPage, setCurrentPage] = useState(1);
   return (
     <>
       {blogs.map((blog) => (
