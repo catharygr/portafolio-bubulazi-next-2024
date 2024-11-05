ebf8da21bb54ff8ca9ff9a5a154007a0c4be3267
catharygr <103951447+catharygr@users.noreply.github.com>
2024-11-05 14:51:57 +0000

    Trabajando en la lógica del componente

--- src/app/componentes/BlogPaginationList/BlogPaginationList.jsx
+++ src/app/componentes/BlogPaginationList/BlogPaginationList.jsx
@@ -1,18 +1,41 @@
 "use client";
-import BlogCard from "../BlogCard";
-import styles from "./BlogPaginationList.module.css";
 import { useState } from "react";
+import BlogCard from "../BlogCard";
+import styles from "./BlogPagination.module.css";
 
 export default function BlogPaginationList({ blogs }) {
   const [currentPage, setCurrentPage] = useState(1);
+  const blogsPerPage = 5;
+
+  // Calcular los blogs a mostrar en la página actual
+  const indexOfLastBlog = currentPage * blogsPerPage;
+  const indexOfFirstBlog = indexOfLastBlog - blogsPerPage;
+  const currentBlogs = blogs.slice(indexOfFirstBlog, indexOfLastBlog);
+
+  // Cambiar de página
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <>
-      {blogs.map((blog) => (
+      {currentBlogs.map((blog) => (
         <BlogCard
           key={blog.slug}
           {...blog}
         />
       ))}
+      <div className={styles.paginacion}>
+        {Array.from(
+          { length: Math.ceil(blogs.length / blogsPerPage) },
+          (_, i) => (
+            <button
+              key={i + 1}
+              onClick={() => paginate(i + 1)}
+            >
+              {i + 1}
+            </button>
+          )
+        )}
+      </div>
     </>
   );
 }
